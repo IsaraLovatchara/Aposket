80541abf6be3b5e7a49c9567c215b23b7dc30311
IsaraLovatchara <isara.l@ku.th>
2023-03-03 05:17:46 +0700

    3/3/23

---
+++ frontend/src/data.js
@@ -0,0 +1,45 @@
+const data = {
+  products: [
+    {
+      name: 'Inuyasha',
+      slug: 'Inuyasha Figure',
+      height: 15,
+      category: 'Anime',
+      image: '/images/f1.jpg',
+      price: 150,
+      countInStock: 10,
+      title: 'Inuyasha',
+      rating: 5,
+      //numReview: 10,
+      description: 'Inuyasha figure',
+    },
+    {
+      name: 'Snuyasha',
+      slug: 'Inuyasha Figure',
+      height: 15,
+      category: 'Anime',
+      image: '/images/f1.JPG',
+      price: 150,
+      countInStock: 10,
+      title: 'Inuyasha',
+      //rating: 5,
+      //numReview: 10,
+      description: 'Inuyasha figure',
+    },
+    {
+      name: 'Ynuyasha',
+      slug: 'Inuyasha Figure',
+      height: 15,
+      category: 'Anime',
+      image: '/images/f1.JPG',
+      price: 150,
+      countInStock: 10,
+      title: 'Inuyasha',
+      rating: 5,
+      //numReview: 10,
+      description: 'Inuyasha figure',
+    },
+  ],
+};
+
+export default data;
